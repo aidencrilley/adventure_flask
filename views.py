1902dc87d20479833142ed69a45b2ba96d7342eb
Aiden Crilley <aidencri@udel.edu>
2019-10-29 12:12:13 -0400

    Added parts list, you turn in parts at end of game for a prize

--- views.py
+++ views.py
@@ -51,10 +51,18 @@ def mclaren(world: dict):
 @simple_route('/mclaren_key')
 def mclaren_key(world: dict):
     world["Has_McLaren_Key"] = True
+    if "Alternator" in world["Parts"]:
+        pass
+    else:
+        world["Parts"].append("Alternator")
     return render_template("mclaren_key.html", world=world)
 
 @simple_route('/lamborghini')
 def lamborghini(world: dict):
+    if "Fuzzy Dice" in world["Parts"]:
+        pass
+    else:
+        world["Parts"].append("Fuzzy Dice")
     return render_template("lamborghini.html", world=world)
 
 @simple_route('/ferrari')
@@ -63,16 +71,28 @@ def ferrari(world: dict):
 
 @simple_route('/720s_test_drive')
 def mclaren_test_drive(world: dict):
+    if "Battery" in world["Parts"]:
+        pass
+    else:
+        world["Parts"].append("Battery")
     return render_template("720s_test_drive.html", world=world)
 
 @simple_route('/get_ticket')
 def get_ticket(world: dict):
+    if "Ticket" in world["Parts"]:
+        pass
+    else:
+        world["Parts"].append("Ticket")
     world["Has_Ticket"] = True
     return render_template("get_ticket.html", world=world)
 
 @simple_route('/ferrari_race')
-def ferrar_race(world: dict):
+def ferrari_race(world: dict):
     return render_template("ferrari_race.html", world=world)
+
+@simple_route('/prize')
+def prize(world: dict):
+    return render_template("prize.html", world=world)
 
 @simple_route('/save/')
 def save_laborghini_track(world: dict, *args) -> str:
@@ -86,6 +106,10 @@ def save_laborghini_track(world: dict, *args) -> str:
 def save_ken_block(world: dict, *args) -> str:
     world["ken_block_autograph"] = request.values.get("signature?")
     if world["ken_block_autograph"] == "Yes":
+        if "Ken Block Autograph" in world["Parts"]:
+            pass
+        else:
+            world["Parts"].append("Ken Block Autograph")
         return render_template("ken_block.html", world=world)
     else:
         return render_template("showroom.html")
